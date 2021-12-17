613c6a31265f476e1bb2d9c3efd5aa80430e9fd2
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2021-12-17 18:56:46 +0100

    Fixed Google Maps Error

--- pages/properties/[id].js
+++ pages/properties/[id].js
@@ -77,7 +77,7 @@ function Property() {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3105.1501993708557!2d-77.03871848432898!3d38.89768045453188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7b7bcdecbb1df%3A0x715969d86d0b76bf!2sBijela%20ku%C4%87a!5e0!3m2!1shr!2shr!4v1639677514826!5m2!1shr!2shr"
         width="100%"
         style={{ border: 0 }}
-        allowfullScreen=""
+        allowFullScreen="true"
         loading="lazy"
       ></iframe>
 
