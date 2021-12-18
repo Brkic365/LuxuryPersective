cc8fce9b56aa2398961c3f1cb88914546b64ac96
Brkic365 <79804244+Brkic365@users.noreply.github.com>
2021-12-18 15:08:18 +0100

    Added mobile menu

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.scss";
 
 import { useRouter } from "next/router";
 
+export function MobileMenu({ open, close }) {
+  return open ? (
+    <div className={styles.mobileMenu}>
+      <img src="/svg/close.svg" onClick={() => close(true)} />
+      <ul>
+        <li>
+          <Link href="/">
+            <a onClick={() => close(true)}>HOME</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about-us">
+            <a onClick={() => close(true)}>ABOUT US</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/properties">
+            <a onClick={() => close(true)}>PROPERTIES</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  ) : null;
+}
+
 function Navbar() {
   const router = useRouter();
 
-  console.log(router.pathname);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className={styles.nav}>
@@ -25,7 +50,14 @@ function Navbar() {
           className={styles.menu}
           src="/svg/menu.svg"
           alt="Mobile Menu Icon"
+          onClick={() => setMobileMenuOpen(true)}
         />
+
+        <MobileMenu
+          open={mobileMenuOpen}
+          close={() => setMobileMenuOpen(false)}
+        />
+
         <div className={styles.links}>
           <Link href="/">
             <a className={router.pathname === "/" && styles.open}>HOME</a>
